70f1077c9ed7580c452e299d52fa419059500755
BillPlayzToday <95703244+BillPlayzToday@users.noreply.github.com>
2023-12-25 11:16:20 +0100

    The Changes

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 import random
 import numpy
+import copy
 
 
 class game2048:
     def __init__(self,resolution = 4):
         self.resolution = resolution
         self.state = numpy.zeros((resolution,resolution),dtype = numpy.int8)
+        self.score = 0
         for i in range(2):
             self._add_new()
 
@@ -20,41 +22,63 @@ class game2048:
     def _add_new(self):
         free_positions = self._get_free_positions()
         if (free_positions == []):
-            return False
+            return
         self.state[random.choice(free_positions)] = numpy.random.choice([1,2],p = [0.75,0.25])
-        return True
+
+    def is_over(self):
+        start_state = copy.deepcopy(self.state)
+        self.swipe_up()
+        if (numpy.array_equal(self.state,start_state)):
+            self.swipe_right()
+            if (numpy.array_equal(self.state,start_state)):
+                self.swipe_down()
+                if (numpy.array_equal(self.state,start_state)):
+                    self.swipe_left()
+                    if (numpy.array_equal(self.state,start_state)):
+                        return True
+        self.state = start_state
+        return False
 
     def swipe_up(self):
         rotated_state = numpy.rot90(self.state,1)
-        self.state = numpy.rot90(_swipe(rotated_state),-1)
-        return self._add_new()
+        swiped_state,score_diff = _swipe(rotated_state)
+        self.score = (self.score + score_diff)
+        self.state = numpy.rot90(swiped_state,-1)
+        self._add_new()
 
     def swipe_right(self):
         rotated_state = numpy.rot90(self.state,2)
-        self.state = numpy.rot90(_swipe(rotated_state),-2)
-        return self._add_new()
+        swiped_state,score_diff = _swipe(rotated_state)
+        self.score = (self.score + score_diff)
+        self.state = numpy.rot90(swiped_state,-2)
+        self._add_new()
 
     def swipe_down(self):
         rotated_state = numpy.rot90(self.state,-1)
-        self.state = numpy.rot90(_swipe(rotated_state),1)
-        return self._add_new()
+        swiped_state,score_diff = _swipe(rotated_state)
+        self.score = (self.score + score_diff)
+        self.state = numpy.rot90(swiped_state,1)
+        self._add_new()
 
     def swipe_left(self):
-        self.state = _swipe(self.state)
-        return self._add_new()
+        swiped_state,score_diff = _swipe(self.state)
+        self.score = (self.score + score_diff)
+        self.state = swiped_state
+        self._add_new()
 
     def print(self):
         for row in self.state:
             row_string = " "
             for value in row:
                 if (value != 0):
-                    print_value = str(2 ** value)
+                    print_value = _number_string(2 ** value)
                 else:
-                    print_value = "X"
+                    print_value = " "
                 row_string = f"{row_string}{print_value} "
             print(row_string)
 
 def _swipe(field):
+    score_diff = 0
     for row_index in range(len(field)):
         row = list(field[row_index])
         stripped_row = []
@@ -66,6 +90,7 @@ def _swipe(field):
         while (index < (len(stripped_row) - 1)):
             if (stripped_row[index] == stripped_row[index + 1]):
                 stripped_row[index] = (stripped_row[index] + 1)
+                score_diff = (score_diff + (2 ** stripped_row[index]))
                 del stripped_row[index + 1]
             index = (index + 1)
 
@@ -74,4 +99,10 @@ def _swipe(field):
         
         field[row_index] = numpy.array(stripped_row,dtype = numpy.int8)
         
-    return field
+    return field,score_diff
+
+def _number_string(number):
+    final_string = str(number)
+    while (len(final_string) < 4):
+        final_string = f" {final_string}"
+    return final_string
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-import random
 import numpy
 import io
 import struct
@@ -10,6 +9,7 @@ buffer_length = 4096
 
 last_game_over = None
 games_per_second = io.BytesIO()
+max_number = -1
 max_score = -1
 
 def clear_terminal():
@@ -29,13 +29,20 @@ def get_average():
     games_per_second.seek(cursor_before)
     return numpy.average(unpacked_array)
 
+def swipe_circle():
+    current_game.swipe_up()
+    current_game.swipe_right()
+    current_game.swipe_down()
+    current_game.swipe_left()
+
 while True:
     current_game = game.game2048()
     while True:
-        swipe_return = random.choice([current_game.swipe_up,current_game.swipe_right,current_game.swipe_down,current_game.swipe_left])()
-        if (not swipe_return):
-            current_score = max(current_game.state.flatten())
-            max_score = max(max_score,current_score)
+        swipe_circle()
+        if (current_game.is_over()):
+            current_number = max(current_game.state.flatten())
+            max_number = max(max_number,current_number)
+            max_score = max(max_score,current_game.score)
             break
     current_time = time.time()
     if (last_game_over):
@@ -46,5 +53,8 @@ while True:
 
     clear_terminal()
     print(f"Games per second: {str(get_average())}")
-    print(f"Max score: {str(2 ** max_score)}")
+    print(f"Max number: {str(2 ** max_number)}")
+    print(f"Max score: {str(max_score)}")
     print(f"Cursor: {str(games_per_second.tell())}")
+    print("----------")
+    current_game.print()
